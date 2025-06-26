ebc8566d1093d30541abe48d94196d88030610ef
defendimattia <defendi.mattia92@gmail.com>
2025-06-26 15:03:45 +0200

    Refactor Cuffie class to include import statement for BigDecimal and improve formatting

--- src/org/lessons/java/shop/Cuffie.java
+++ src/org/lessons/java/shop/Cuffie.java
@@ -1,9 +1,15 @@
 package org.lessons.java.shop;
 
-public class Cuffie extends Prodotto{
+import java.math.BigDecimal;
+
+public class Cuffie extends Prodotto {
     private String color;
     private boolean isWireless;
 
-
+    public Cuffie(String name, String brand, BigDecimal price, BigDecimal iva, String color, boolean isWireless) {
+        super(name, brand, price, iva);
+        this.color = color;
+        this.isWireless = isWireless;
+    }
 
 }
