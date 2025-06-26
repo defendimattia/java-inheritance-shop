eee8ffc42e6b97a310ab715961f66985ce4b4cf4
defendimattia <defendi.mattia92@gmail.com>
2025-06-26 15:00:31 +0200

    Refactor Televisori constructor for improved readability and formatting

--- src/org/lessons/java/shop/Televisori.java
+++ src/org/lessons/java/shop/Televisori.java
@@ -7,11 +7,35 @@ public class Televisori extends Prodotto {
     private int height;
     private boolean isSmart;
 
-    public Televisori (String name, String brand, BigDecimal price, BigDecimal iva, int width, int height, boolean isSmart) {
-        super( name,  brand,  price, iva );
+    public Televisori(String name, String brand, BigDecimal price, BigDecimal iva, int width, int height,
+            boolean isSmart) {
+        super(name, brand, price, iva);
         this.width = width;
         this.height = height;
         this.isSmart = isSmart;
-        
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public boolean IsSmart() {
+        return this.isSmart;
+    }
+
+    public void setSmart(boolean isSmart) {
+        this.isSmart = isSmart;
     }
 }
