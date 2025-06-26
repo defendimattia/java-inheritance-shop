e77d4a450dc5fdc2dc43ccca9adcd9617e51634f
defendimattia <defendi.mattia92@gmail.com>
2025-06-26 14:24:23 +0200

    Add fields to Prodotto class for product attributes

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,12 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+
 public class Prodotto {
+    protected String code;
+    protected String name;
+    protected String brand;
+    protected BigDecimal price;
+    protected BigDecimal iva;
 
 }
