78424cf5265965b8fe1df2891badee3527d85a1c
defendimattia <defendi.mattia92@gmail.com>
2025-06-26 14:55:34 +0200

    Refactor Televisori class to include import statement for BigDecimal and improve formatting

--- src/org/lessons/java/shop/Televisori.java
+++ src/org/lessons/java/shop/Televisori.java
@@ -1,9 +1,17 @@
 package org.lessons.java.shop;
 
-public class Televisori extends Prodotto{
+import java.math.BigDecimal;
+
+public class Televisori extends Prodotto {
     private int width;
     private int height;
     private boolean isSmart;
 
-    
+    public Televisori (String name, String brand, BigDecimal price, BigDecimal iva, int width, int height, boolean isSmart) {
+        super( name,  brand,  price, iva );
+        this.width = width;
+        this.height = height;
+        this.isSmart = isSmart;
+        
+    }
 }
